b2f53a11f2a7f508471e675f18428a2199d42019
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2026-02-23 14:47:00 +0100

    Initialize new python project with "uv init", rename files & do some cleanup of old files

---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 2dv50e-bachelor-thesis!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/services/print_service.py
@@ -0,0 +1,1 @@
+# This service is used to print the results of the model to the console.
